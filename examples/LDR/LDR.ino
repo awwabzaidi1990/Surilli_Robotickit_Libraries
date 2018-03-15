8f831e8a1c22c33f01d1f7a7afff5da0f35bf917
Muhammad Umair <31646100+silverbac@users.noreply.github.com>
2018-03-15 15:48:54 +0500

    Update LDR.ino

--- examples/LDR/LDR.ino
+++ examples/LDR/LDR.ino
@@ -1,1 +1,22 @@
+#define LDRpin A0    // pin where we connected the LDR and the resistor
+int LDRValue = 0;     // result of reading the analog pin
+
+void setup() 
+{
+    SerialUSB.begin(9600); // sets serial port for communication
+}
+void loop() 
+{
+  LDRValue = analogRead(LDRpin);   // read the value from the LDR
+ // Serial.println(LDRValue);      // print the value to the serial port
+  delay(1000);                     // wait a little
+if(LDRValue<=450)
+{
+  SerialUSB.println("Mode: Night");
+  }
+else if(LDRValue>=451)
+{
+  SerialUSB.println("Mode: Day");
+  } 
+}
 
