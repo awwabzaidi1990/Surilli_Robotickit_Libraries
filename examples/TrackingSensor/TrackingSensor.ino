f3712bb92f21241277c0a42c289a074dead6d987
Muhammad Umair <31646100+silverbac@users.noreply.github.com>
2018-03-26 10:59:23 +0500

    Update TrackingSensor.ino

--- examples/TrackingSensor/TrackingSensor.ino
+++ examples/TrackingSensor/TrackingSensor.ino
@@ -1,1 +1,16 @@
+int tracking_sensor_pin =10;
+void setup() {
+  pinMode(tracking_sensor_pin , INPUT);
+  SerialUSB.begin(9600);
+}
+void loop() {
+  delay(200);
+  int TrackingSensor = digitalRead(tracking_sensor_pin);
+  if(TrackingSensor == 1){
+   SerialUSB.println("Obstacle not detected ");  
+  }
+  else if(TrackingSensor == 0){
+   SerialUSB.println("Obstacle detected "); 
+  }
+}
 
