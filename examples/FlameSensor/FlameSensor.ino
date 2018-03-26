eeb064baeae30570fdc60f20bdc515056b56ac2c
Muhammad Umair <31646100+silverbac@users.noreply.github.com>
2018-03-26 10:56:17 +0500

    Update FlameSensor.ino

--- examples/FlameSensor/FlameSensor.ino
+++ examples/FlameSensor/FlameSensor.ino
@@ -1,1 +1,15 @@
-
+int flame_sensor_pin =10;
+void setup() {
+  pinMode(flame_sensor_pin, INPUT);
+  SerialUSB.begin(9600);
+}
+void loop() {
+  delay(500);
+  int FlameSensor = digitalRead(flame_sensor_pin );
+  if(FlameSensor == 1){
+   SerialUSB.println("Flame not detected ");  
+  }
+  else if(FlameSensor == 0){
+   SerialUSB.println("Flame detected "); 
+  }
+}
